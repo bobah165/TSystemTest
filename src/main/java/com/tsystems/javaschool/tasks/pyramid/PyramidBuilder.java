12434a632d8bb07765d5f1c073e75eda9dc37747
bobah165 <43619200+bobah165@users.noreply.github.com>
2019-06-05 19:37:09 +0300

    commit1

---
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -0,0 +1,80 @@
+package com.tsystems.javaschool.tasks.pyramid;
+
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class PyramidBuilder {
+
+    public int[][] buildPyramid(List<Integer> inputNumbers) {
+
+        int numberOfRows=0;
+        int numberOfColumn = 0;
+        int count=inputNumbers.size();
+        int elementInList = 0;
+        int[][] array;
+
+        for(Integer to:inputNumbers){
+            if(to==null)
+                throw new CannotBuildPyramidException();
+        }
+
+        if (inputNumbers.isEmpty()) throw new CannotBuildPyramidException();
+
+
+
+        while ((count=count-numberOfRows)!=0) {
+            numberOfRows++;
+        }
+
+        if (numberOfColumn<0||numberOfRows<0) throw new CannotBuildPyramidException();
+
+        numberOfColumn = 2*numberOfRows-1;
+
+
+        try {
+            array = new int[numberOfRows][numberOfColumn];
+        } catch (OutOfMemoryError error) {
+            throw new CannotBuildPyramidException();
+        }
+
+        Collections.sort(inputNumbers);
+
+        for (int i=0;i<numberOfRows;i++){
+            if (i==0) {
+                array[i][numberOfRows-(i+1)] = inputNumbers.get(elementInList);
+                elementInList++;
+                continue;
+            }
+            else {
+                array[i][numberOfRows-(i+1)] = inputNumbers.get(elementInList);
+                elementInList++;
+            }
+
+            if(i!=numberOfRows-1) {
+                for (int j = numberOfRows; j <= numberOfColumn - (numberOfRows - (i + 1)); j++) {
+                    if (array[i][j - i] == 0) {
+                        array[i][j - (i - 1)] = inputNumbers.get(elementInList);
+                        elementInList++;
+                    }
+                }
+                if (i > 2) {
+                    array[i][numberOfColumn - (numberOfRows - i)] = inputNumbers.get(elementInList);
+                    elementInList++;
+                }
+            }
+            else {
+                elementInList--;
+                for(int j=0;j<numberOfColumn;j++){
+                    if((j+1)%2!=0&&elementInList<inputNumbers.size()) {
+                        array[i][j]=inputNumbers.get(elementInList);
+                        elementInList++;
+                    }
+                }
+            }
+        }
+
+        return array;
+    }
+}
